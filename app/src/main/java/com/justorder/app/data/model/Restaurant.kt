af4504a58b1f691f9fce6fe28eedc196c4e3a1c2
Sadaque Khan <Sadaque.Khan@fdmgroup.com>
2025-03-16 18:21:17 +0000

    Add network files and restaurant model with recent modifications

---
+++ app/src/main/java/com/justorder/app/data/model/Restaurant.kt
@@ -0,0 +1,15 @@
+package com.justorder.app.data.model
+
+
+data class Restaurant(
+    val name: String,
+    val rating: Float,
+    val cuisineTypes: List<String>,
+    val logoUrl: String?
+)
+
+data class RestaurantResponse(
+
+    val restaurant: List<Restaurant>
+)
+
